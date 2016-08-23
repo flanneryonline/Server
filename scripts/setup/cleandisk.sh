1c1331507c84878175332cf39a2a676759eabace
Pat Flannery <pat@flanneryonline.com>
2016-08-23 15:32:50 -0500

    Cleanup.

--- scripts/setup/cleandisk.sh
+++ scripts/setup/cleandisk.sh
@@ -1,14 +1,23 @@
-#!/usr/local/env bash
+#!/usr/bin/env bash
 
 set -o errexit
 set -o nounset
 
-#check if drive exists
+# No args? More than 1 arg?
+if [[ $# -ne 1 ]]
+then
+    exit 1
+fi
+
+DRIVE=$1
+
+# Check if arg is a drive
 if [[ ! -f /dev/${DRIVE} ]]
 then 
     exit 1
 fi
 
+# MAX is the number of partitions that need to be removed before we can destroy the disk
 MAX=$(gpart show ${DRIVE} | awk '{if ($3>0 && $3<128){max=$3}} END{print max}')
 if [[ "${MAX}" =~ "[^0-9]+" ]]
 then
