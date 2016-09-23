ef9cedef5c3e3127c7876990440979c70389869c
Pat Flannery <pat@flanneryonline.com>
2016-09-23 10:26:53 -0500

    removing plug and scripting download

---
+++ dot.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+ln -s server/dotfiles/dot.vim ../.vim
+ln -s server/dotfiles/dot.vimrc ../.vimrc
+ln -s server/dotfiles/dot.zshrc ../.zshrc
+ln -s server/dotfiles/dot.tmux.conf ../.tmux.conf
+
