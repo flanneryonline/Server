86fd8224f9a5974c8fbd03cbada9e31dcead6221
Pat Flannery <pat@flanneryonline.com>
2016-10-06 10:02:19 -0500

    lots of changes

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,9 +6,37 @@
 set -o errexit
 set -o nounset
 
-source ~/server/scripts/setup/setup_base_install
+freebsd_version="11.0-RELEASE"
+altroot="/mnt"
+jail_dir="${altroot}/usr/jails"
+gateway="10.0.0.1"
+domain="flanneryonline.com"
+hostname="hostserver"
 
-base_install $(uname -a) "10.3-RELEASE" "${ALTROOT}" 
+source ~/server/scripts/setup/setup_zfs_init
+source ~/server/scripts/setup/setup_system_base
+source ~/server/scripts/setup/setup_jail_base
+source ~/server/scripts/setup/setup_jail_create
+
+zfs_init \
+    "" \        #root disks
+    "" \        #fast disks
+    "${altroot}"
+system_base "$(uname -a)" "${freebsd_version}" "${altroot}"
+jail_base "${freebsd_version}" "${jail_dir}"
+
+cd "${altroot}"
+ln -s usr/home home
+ln -s root usr/home/root
+
+sysrc -R "${ALTROOT}" zfs_enable="YES"
+sysrc -R "${ALTROOT}" defaultrouter="${gateway}"
+sysrc -R "${ALTROOT}" sendmail_submit_enable="NO"
+sysrc -R "${ALTROOT}" sendmail_outbound_enable="NO"
+sysrc -R "${ALTROOT}" sendmail_msp_queue_enable="NO"
+sysrc -R "${ALTROOT}" hostname="${hostname}${test}.${domain}"
+
+
 
 exit 0
 
