2dc2c1b403cb50e2f3d3c96356c49c4def9b3d27
Patrick Flannery <Patrick Flannery>
2017-02-05 12:27:29 -0600

    formatting

--- include/README.md
+++
@@ -1,3 +0,0 @@
-# Include Scripts
-
-These are scripts that are not executable directly. This should be sourced from another file.
--- periodic/daily/zfs_rolling_daily.sh
+++ periodic/daily/zfs_rolling_daily.sh
@@ -1,14 +1,11 @@
-#!/usr/bin/env bash
-
-set -o errexit
-set -o nounset
+#!/bin/sh
 
 zfs destroy -r zroot@yesterday >/dev/null 2>&1
 zfs destroy -r zfast@yesterday >/dev/null 2>&1
 zfs destroy -r zstorage@yesterday >/dev/null 2>&1
 
-zfs rename -r zroot@today @yesterday >/dev/null 2>&1
 zfs rename -r zfast@today @yesterday >/dev/null 2>&1
+zfs rename -r zroot@today @yesterday >/dev/null 2>&1
 zfs rename -r zstorage@today @yesterday >/dev/null 2>&1
 
 zfs snapshot -r zroot@today >/dev/null 2>&1
