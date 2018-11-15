ad7ce66a1cf0091eb67485ff81be2d6d8e04c66e
Pat Flannery <pat@flanneryonline.com>
2018-11-15 10:54:51 -0600

    add emby + some fixes

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # [ -d /opt/server ] && rm -r /opt/server
 # git clone https://github.com/flanneryonline/server.git /opt/server && cd /opt/server
-# chmod +x install.sh && SYNC_DATA=0 LINK_BACKUP=1 ./install.sh |& tee /var/log/debootstrap.log
+# chmod +x install.sh && ./install.sh |& tee /var/log/debootstrap.log
 
 export DEBIAN_FRONTEND=noninteractive
 
@@ -13,16 +13,9 @@ SERVER_INSTALL=${SERVER_INSTALL:-/opt/server}
 root=/mnt/install
 chroot_eval="chroot "$root" /usr/bin/env PATH=/usr/sbin:/usr/bin/:/bin:/sbin DEBIAN_FRONTEND=noninteractive"
 
-SYNC_DATA=${SYNC_DATA:-1}
 FAST_STORAGE_ENABLED=${FAST_STORAGE_ENABLED:-1}
 SLOW_STORAGE_ENABLED=${SLOW_STORAGE_ENABLED:-1}
-SERVICES_ENABLED=${SERVICES_ENABLED:-1}
 BACKUP_ENABLED=${BACKUP_ENABLED:-1}
-LINK_BACKUP=${LINK_BACKUP:-0}
-
-[ $BACKUP_ENABLED -eq 0 ] && SYNC_DATA=0
-[ $BACKUP_ENABLED -eq 0 ] && SERVICES_ENABLED=0
-[ $BACKUP_ENABLED -eq 0 ] && LINK_BACKUP=0
 
 admin_password=$(whiptail --title "Set $ADMIN_USERNAME password" --passwordbox "Please enter password for user $ADMIN_USERNAME:" 0 10 2>&1 >/dev/tty)
 wt_boot='whiptail --title "Choose All Boot Disks" --checklist "Boot disks will be ERASED!" 0 10 0 '
